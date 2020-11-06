0e79df9075193ae450b4630d14d4d2112fe91d5c
alligatormonday <joey@joeyjepson.com>
2020-11-05 21:55:21 -0700

    Created seeds file.

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Impossible Burger")
+
+INSERT INTO burgers (burger_name)
+VALUES ("Double-Double")
+
+INSERT INTO burgers (burger_name)
+VALUES ("White-Castle Slider")
