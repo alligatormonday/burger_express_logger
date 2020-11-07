72096cdf56c9fa7cdba0bbb4205eebbad73633af
alligatormonday <joey@joeyjepson.com>
2020-11-07 14:31:23 -0700

    Added JAWSDB_URL environmental variable and deployed to Heroku.

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,26 @@
 // Set up MySQL connection.
 const mysql = require("mysql");
+let connection
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Limetree001!",
-    database: "burgers_db"
-});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Limetree001!",
+        database: "burgers_db"
+    });
+};
+
+
+
+
+
+
+
 
 // Make connection.
 connection.connect(function (err) {
@@ -17,6 +30,9 @@ connection.connect(function (err) {
     }
     console.log("connected as id " + connection.threadId);
 });
+
+
+
 
 // Export connection for our ORM to use.
 module.exports = connection;
